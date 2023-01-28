3c16a89f3541a6978f168e377c4080dd579ae59d
mrkleiner <megaadrenaline1055@gmail.com>
2023-01-28 23:56:27 +0100

    update smt to have the antivirus

--- smdtool/smdt.py
+++ smdtool/smdt.py
@@ -84,36 +84,110 @@ class smd:
 
 
 def fix_val(src, lmap):
-		if src.startswith('1.0') or lmap.startswith('1.0'):
-			return src
+		# if src.startswith('1.0') or lmap.startswith('1.0'):
+		# 	return src
+		if src.startswith('1.0') and lmap.startswith('1.0'):
+			return '1.00009999'
 
 		src_num = src[0:6]
 		lmap_num = lmap[2:7]
 		lmap_end = int(lmap_num[-1])
 
 		if lmap_end >= 5 and lmap_num.startswith('0000'):
-			lmap_num = '0010'
+			lmap_num = '0001'
 		else:
 			lmap_num = lmap_num[:-1]
 
 		return src_num + lmap_num
 
 
+def antivirus(tgt_smd):
+
+	tgt_smd = Path(tgt_smd).absolute()
+
+	tris_reverse = smd(tgt_smd)
+
+	new_smd = ''
+	lmap_smd = ''
+
+	# 'v': (pos3, nrm3, uv2, lead)
+	new_tris = ''
+	lmap_tris = ''
+	for tri in tris_reverse.tris:
+		this_tri = tris_reverse.tris[tri]
+		# lmap_tri = tris_lmap.tris[tri]
+		new_tris += this_tri['mat'] + '\n'; lmap_tris += this_tri['mat'] + '\n'
+
+		# fix verts
+		for vert in ('v1','v2','v3',):
+			this_vert = this_tri[vert]
+			# lmap_vert = lmap_tri[vert]
+
+			# write a zero
+			new_tris += '0'; lmap_tris += '0'
+			new_tris += '  '; lmap_tris += '  '
+			# write pos
+			new_tris += ' '.join(this_vert[0]); lmap_tris += ' '.join(this_vert[0])
+			new_tris += '  '; lmap_tris += '  '
+			# write normal
+			new_tris += ' '.join(this_vert[1]); lmap_tris += ' '.join(this_vert[1])
+			new_tris += '  '; lmap_tris += '  '
+			# write reversed UV
+			new_tris += this_vert[2][0][0:6]
+			lmap_tris += '0.' + this_vert[2][0][6:10]
+			new_tris += ' '; lmap_tris += ' '
+			new_tris += this_vert[2][1][0:6]
+			lmap_tris += '0.' + this_vert[2][1][6:10]
+			new_tris += ' '; lmap_tris += ' '
+			# leading bone data
+			# todo: stupid assumption that the entirety of leading data is separated with singular space ' '
+			new_tris += ' '.join(this_vert[3]); lmap_tris += ' '.join(this_vert[3])
+			new_tris += '\n'; lmap_tris += '\n'
+
+	new_smd += tris_reverse.prefix; lmap_smd += tris_reverse.prefix
+	new_smd += 'triangles\n'; lmap_smd += 'triangles\n'
+	new_smd += new_tris.strip(); lmap_smd += lmap_tris.strip()
+	new_smd += '\nend\n'; lmap_smd += '\nend\n'
+	new_smd += tris_reverse.postfix; lmap_smd += tris_reverse.postfix
+
+	(tgt_smd.parent / f'{tgt_smd.stem}_src_reversed.smd').write_text(new_smd)
+	(tgt_smd.parent / f'{tgt_smd.stem}_lightmap_reversed.smd').write_text(lmap_smd)
+
+
+
+
+
+
 def exec():
 
 	if len(sys.argv) < 3:
 		print('Usage:')
-		print('smdt.exe SRC_SMD LIGHTMAP_SMD RESULT_NAME;')
+		print('smdt.exe SRC_SMD LIGHTMAP_SMD RESULT_NAME')
 		print('smdt.exe "E:/sex/sphere_normal.smd" "E:/sex/sphere_lightmap.smd" pootis')
 		print("""(Don't forget the quotes " ")""")
 		print('Converted smd is saved to the same dir the SRC_SMD is in.')
 		print('If no RESULT_NAME specified - a default suffix _conv is added:')
-		print('sphere_normal.smd -> sphere_normal_conv.smd')
+		print('Blue for the virus. Green for the antivirus')
+		print("""There's a cure""")
+		print('The process can be reversed:')
+		print('Type -reverse instead of the LIGHTMAP_SMD to split the SMD back')
+		print('smdt.exe "E:/sex/sphere.smd" -reverse')
+		print('Get it? Get the reference? Let me know...')
 		return
 
+	inp1 = sys.argv[1].strip('"')
+	inp2 = sys.argv[2].strip('"')
 
-	smd_src = Path(sys.argv[1].strip('"'))
-	smd_lmap = Path(sys.argv[2].strip('"'))
+	if inp2 == '-reverse':
+		if not Path(inp1).is_file():
+			print("""The target SMD does not exist. Aborting (I support abortion and everyone who doesn't agree can go fuck themselves)""")
+			return
+		antivirus(inp1)
+		print('Finished reversing')
+		return
+
+	smd_src = Path(inp1)
+	smd_lmap = Path(inp2)
 
 	if False in (smd_src.is_file(), smd_lmap.is_file()):
 		print('One of the smd files does not exist. Aborting')
@@ -122,7 +196,9 @@ def exec():
 	tris_src = smd(smd_src)
 	tris_lmap = smd(smd_lmap)
 
-	assert (len(tris_src.tris) == len(tris_lmap.tris)), 'Models have different triangle count. Wtf. Aborting...'
+	if len(tris_src.tris) != len(tris_lmap.tris):
+		print('Models have different triangle count. Wtf. Aborting...')
+		return
 
 	new_smd = ''
 
@@ -152,6 +228,8 @@ def exec():
 			new_tris += ' '
 			new_tris += fix_val(this_vert[2][1], lmap_vert[2][1])
 			new_tris += ' '
+			# leading bone data
+			# todo: stupid assumption that the entirety of leading data is separated with singular space ' '
 			new_tris += ' '.join(this_vert[3])
 			new_tris += '\n'
 
